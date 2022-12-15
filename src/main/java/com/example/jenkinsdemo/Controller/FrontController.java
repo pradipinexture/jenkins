ae58a39fa4996da67039135281af9ef29d3a9afc
Pradip Chavda <100900297+pradipinexture@users.noreply.github.com>
2022-12-15 11:47:39 +0530

    Fourth commit and Sixth build
    Fourth commit and Sixth build

--- src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
+++ src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
@@ -1,11 +1,17 @@
 package com.example.jenkinsdemo.Controller;
 
 import org.springframework.stereotype.Controller;
+import javax.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontController {
 
+     @PostConstruct
+    public void init(){
+        System.out.println("Hello Pradip, This is sixth build");
+    }
+    
     @GetMapping("/getHome")
     public String homePage(){
         System.out.println("Hello Pradip This is third commit.");
