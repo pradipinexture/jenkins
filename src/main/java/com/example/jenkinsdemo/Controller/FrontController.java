773fad838e537ae164380bfd15d75cbe5879e02e
Pradip Chavda <100900297+pradipinexture@users.noreply.github.com>
2022-12-15 18:17:23 +0530

    added import statement

--- src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
+++ src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
@@ -1,13 +1,13 @@
 package com.example.jenkinsdemo.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontController {
-    
+
     @GetMapping("/getHome")
     public String homePage(){
-        System.out.println("Hello Pradip This is third commit.");
         return "home";
     }
 
