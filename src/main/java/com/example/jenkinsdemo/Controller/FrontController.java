a6c5aab8a195469990a83d464f5035ba148b6639
Pradip Chavda <100900297+pradipinexture@users.noreply.github.com>
2022-12-15 18:26:36 +0530

    error

--- src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
+++ src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
@@ -1,7 +1,5 @@
 package com.example.jenkinsdemo.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontController {
