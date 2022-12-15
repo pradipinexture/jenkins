df09b073050d7f4ff7d195894c6750eb07586571
Pradip Chavda <100900297+pradipinexture@users.noreply.github.com>
2022-12-15 18:29:04 +0530

    error resolve

--- src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
+++ src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
@@ -1,5 +1,8 @@
 package com.example.jenkinsdemo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class FrontController {
