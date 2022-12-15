609e2b4a074443d534222d9a627463c2b57db3be
Pradip Chavda <100900297+pradipinexture@users.noreply.github.com>
2022-12-15 18:13:52 +0530

    removed import statement

--- src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
+++ src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
@@ -1,7 +1,5 @@
 package com.example.jenkinsdemo.Controller;
 
-import org.springframework.stereotype.Controller;
-import javax.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
