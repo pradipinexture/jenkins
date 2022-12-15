4390573121ea48a438069327439f349b7378f265
Pradip Chavda <100900297+pradipinexture@users.noreply.github.com>
2022-12-15 13:27:45 +0530

    New Commit

--- src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
+++ src/main/java/com/example/jenkinsdemo/Controller/FrontController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontController {
-
-     @PostConstruct
-    public void init(){
-        System.out.println("Hello Pradip, This is sixth build");
-    }
     
     @GetMapping("/getHome")
     public String homePage(){
